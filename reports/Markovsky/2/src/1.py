39acaa4cdb570cfc000dd58e5f61284c6ee4ae74
Carrotist <romark99@gmail.com>
2026-02-19 13:56:37 +0300

    feat: add __str__, __repr__ methods

--- reports/Markovsky/2/src/1.py
+++ reports/Markovsky/2/src/1.py
@@ -1,4 +1,12 @@
 class IntegerSet:
-    def __init__(self, max_size, elements=[]):
+    def __init__(self, max_size, elements=None):
+        if elements is None:
+            elements = []
         self.max_size = max_size
         self.elements = elements
+
+    def __str__(self):
+        return f"{self.elements}"
+
+    def __repr__(self):
+        return f"IntegerSet(max_size={self.max_size}, elements={self.elements})"
