f74038506bde782eb35eef29222cf22b35169de3
UnderSann <alexbond2511@gmail.com>
2026-02-20 08:43:32 +0300

    feat: task 1.2),2.2).

---
+++ reports/Bondarchuk/1/src/1.py
@@ -0,0 +1,14 @@
+def product(nums) :
+    if len(nums) == 0:
+        return  0
+    prod = 1
+    for num in nums:
+        prod*=num
+    return prod
+
+if __name__ == '__main__':
+    mas = list(map(int,(input("mas: ").split())))
+    print("max: ",max(mas))
+    print("min: ",min(mas))
+    print("sum: ",sum(mas))
+    print("prod: ",product(mas))
